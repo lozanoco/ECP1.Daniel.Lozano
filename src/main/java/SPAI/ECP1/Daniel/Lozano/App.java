7c930b390025d2eedd252bd9a8efea7f8005d059
Daniel Lozano <lozanoco@gmail.com>
2014-10-10 13:40:21 +0200

    CR1.04

--- src/main/java/SPAI/ECP1/Daniel/Lozano/App.java
+++ src/main/java/SPAI/ECP1/Daniel/Lozano/App.java
@@ -14,6 +14,5 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
     }
 }
