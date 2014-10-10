92c350587a271473a06244e58b55651719cebd4e
Daniel Lozano <lozanoco@gmail.com>
2014-10-10 13:30:22 +0200

    Daniel Lozano

---
+++ src/main/java/SPAI/ECP1/Daniel/Lozano/App.java
@@ -0,0 +1,13 @@
+package SPAI.ECP1.Daniel.Lozano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
